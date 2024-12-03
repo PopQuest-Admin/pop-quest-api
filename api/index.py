c21ae82ced5fc00263c4ba9ff809281e447daeef
VishalRashmika <vishal.rashmika.perera@gmail.com>
2024-12-03 19:26:26 +0530

    google sheet read

--- api/index.py
+++ api/index.py
@@ -4,27 +4,35 @@ import requests
 app = Flask(__name__)
 
 def check_google_sheet(link):
-    prefixes = ["https://","http://"]
-    for pre in prefixes:
-        link = link.strip(pre)
-        if link.startswith("docs.google.com"):
-            parts = str(link).split("/")
-            if parts[1] == "spreadsheets" and parts[2] == "d":
-                data = {
-                    "vailidity" : True
-                }
-                return jsonify(data)
+    try:
+        prefixes = ["https://","http://"]
+        for pre in prefixes:
+            link = link.strip(pre)
+            if link.startswith("docs.google.com"):
+                parts = str(link).split("/")
+                if parts[1] == "spreadsheets" and parts[2] == "d":
+                    return True
+                else:
+                    return False
             else:
-                data = {
-                    "vailidity" : False
-                }
-                return jsonify(data)
-        else:
-            data = {
-                "vailidity" : False
-            }
-            return jsonify(data)
+                return False
+    except:
+        return "ERROR IN : check google sheet func"
+        
 
+def read_google_sheet(url, api_key):
+    try:
+        parts = str(url).split('/')
+        sheet_id = parts[5]
+        sheet_name = "Sheet1"
+        template_url = f"https://sheets.googleapis.com/v4/spreadsheets/{sheet_id}/values/{sheet_name}!A1:Z?alt=json&key={api_key}"
+        response  = requests.get(template_url)
+
+        data = response.json()
+        return data
+    
+    except:
+        return "Error occured"
 
 @app.route('/')
 def home():
@@ -33,14 +41,22 @@ def home():
 @app.route('/checksheet', methods=['GET'])
 def validate():
     link = request.args.get('link')
-    return check_google_sheet(link)
+    data = {
+        "validity" : check_google_sheet(link)
+    }
+    return jsonify(data)
+
+@app.route('/readsheet', methods=['GET'])
+def readdata():
+    sheet_url = request.args.get('url')
+    api_key = request.args.get('api_key')
+    
+    return jsonify(read_google_sheet(sheet_url,api_key))
 
 @app.route('/test')
 def test():
-    response = requests.get("https://www.instructables.com/HyperDuino-based-CubeSat/")
-    return response.text
+    return "Welcome to the test route"
 
 
-# for local dev only
 if __name__ == '__main__':
     app.run()
